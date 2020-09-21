4c62dbab17b1b95b635f8286996187e2c3a7bf02
aslepenkov <alexmichel249@gmail.com>
2020-09-21 18:19:38 +0700

    init

---
+++ nav.js
@@ -0,0 +1,17 @@
+const about = document.getElementById("about");
+const study = document.getElementById("study");
+const work = document.getElementById("work");
+const hobby = document.getElementById("hobby");
+
+const aboutContent = document.getElementById("aboutContent");
+const studyContent = document.getElementById("studyContent");
+const workContent = document.getElementById("workContent");
+const hobbyContent = document.getElementById("hobbyContent");
+
+const pages = [
+  { nav: about, page: aboutContent },
+  { nav: study, page: studyContent },
+  { nav: work, page: workContent },
+  { nav: hobby, page: hobbyContent },
+];
+
