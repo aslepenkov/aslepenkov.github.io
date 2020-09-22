6014af66670d9030114c43b716ab0b26964e41cf
aslepenkov <alexmichel249@gmail.com>
2020-09-22 18:20:04 +0700

    js

--- nav.js
+++ nav.js
@@ -15,3 +15,27 @@ const pages = [
   { nav: hobby, page: hobbyContent },
 ];
 
+pages.map((p) => {
+  p.nav.onclick = () => {
+    //ENABLE SELECTED NAVBAR BUTTON/PAGE
+    if (!p.nav.classList.contains("active") && p.page.classList.contains("hidden")) {
+      //UPDATE NAVBAR
+      p.nav.classList.add("active");
+
+      //UPDATE PAGE CONTENT
+      p.page.classList.remove("hidden");
+    }
+
+    //DISABLE OTHER NAVBAR BUTTONS/PAGES
+    pages
+      .filter((f) => p.nav !== f.nav)
+      .forEach((element) => {
+        if (element.nav.classList.contains("active") && !p.page.classList.contains("hidden")) {
+          //UPDATE NAVBAR
+          element.nav.classList.remove("active");
+          //UPDATE PAGE CONTENT
+          element.page.classList.add("hidden");
+        }
+      });
+  };
+});
